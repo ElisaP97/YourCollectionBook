524173f92ce38ccd7a1b4aebe1f8bf1b2d605f83
Elisa Poggi <elisapoggi97@gmail.com>
2024-06-29 00:41:06 +0200

    Aggiunto readme e licenza

--- README.md
+++ README.md
@@ -1,29 +1,47 @@
 # NerdList 
 
-This template should help get you started developing with Vue 3 in Vite.
+E' un'applicazione web che serve a tenere traccia della propria libreria (volumi, titoli, autori), tenere traccia delle letture in corso o da iniziare o completate e permette di creare e gestire un elenco della spesa.
+Questo progetto è nato da un esercizio di creazione di una lista della spesa per prendere confidenza con VueJs v.3.
+
+## Layout
+Per realizzare il layout del progetto è stato utilizzato:
+- Tailwind css
+- Daisy UI
+
+## Salvataggio dati
+Per la memorizzazione dei dati è stato utilizzato un tool "json-server"
 
 ## Recommended IDE Setup
 
 [VSCode](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) (and disable Vetur).
 
-## Customize configuration
+## Risorse:
+- [Vue Mastery](https://www.vuemastery.com/)
+- [Documentazione ufficiale](https://vuejs.org/)
+- [Tailwind css](https://tailwindcss.com/)
+- [Daisy UI](https://daisyui.com/)
+- [axios js](https://axios-http.com/docs/intro)
+- [json-server](https://github.com/typicode/json-server/tree/v0)
 
-See [Vite Configuration Reference](https://vitejs.dev/config/).
+#Licenza
+MIT License
 
-## Project Setup
+Copyright (c) [2024] [Poggi Elisa]
 
-```sh
-npm install
-```
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
 
-### Compile and Hot-Reload for Development
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
 
-```sh
-npm run dev
-```
-
-### Compile and Minify for Production
-
-```sh
-npm run build
-```
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- src/components/Collection.vue
+++ src/components/Collection.vue
@@ -1,4 +1,23 @@
 <script setup>
+    //componenti importati 
+
+    //librerie e funzioni importate
+    import axios from 'axios';
+    import { onMounted } from 'vue';
+    import { ref } from 'vue';
+    import { computed } from 'vue';
+
+    //logica
+
+    const urlCollection= "http://localhost:3000/mangacollection";
+    const books = ref([]);
+
+    onMounted(()=> {
+        axios.get(urlCollection).then(resp=>{
+            console.log(resp);
+            books.value=resp.data;
+        })
+    })
 
 </script>
 
@@ -50,13 +69,15 @@
                                 <th></th>
                             </tr>
                         </thead>
-                        <tbody>
+                        <tbody v-for="book in books" :key="book.id">
                         <!-- row 1 -->
                         <tr>
                             <td>
+                                <span v-if="book.reading===1">
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
                                 <path fill-rule="evenodd" d="M8.603 3.799A4.49 4.49 0 0 1 12 2.25c1.357 0 2.573.6 3.397 1.549a4.49 4.49 0 0 1 3.498 1.307 4.491 4.491 0 0 1 1.307 3.497A4.49 4.49 0 0 1 21.75 12a4.49 4.49 0 0 1-1.549 3.397 4.491 4.491 0 0 1-1.307 3.497 4.491 4.491 0 0 1-3.497 1.307A4.49 4.49 0 0 1 12 21.75a4.49 4.49 0 0 1-3.397-1.549 4.49 4.49 0 0 1-3.498-1.306 4.491 4.491 0 0 1-1.307-3.498A4.49 4.49 0 0 1 2.25 12c0-1.357.6-2.573 1.549-3.397a4.49 4.49 0 0 1 1.307-3.497 4.49 4.49 0 0 1 3.497-1.307Zm7.007 6.387a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z" clip-rule="evenodd" />
                                 </svg>
+                                </span>
                             </td>
                             <td>
                                 <div class="flex items-center gap-3">
@@ -68,24 +89,24 @@
                                         </div>
                                     </div> -->
                                     <div>
-                                    <div class="font-bold">Bleach</div>
-                                        <div class="text-sm opacity-50">Tite kubo</div>
+                                    <div class="font-bold">{{book.title}}</div>
+                                        <div class="text-sm opacity-50">{{book.author}}</div>
                                     </div>
                                 </div>
                             </td>
                             <td>
-                                74/74
+                                {{book.volumes}}
                             </td>
                             <td>
-                                Completa
+                                {{book.status}}
                             </td>
                             <td>
-                                <div class="rating">
-                                    <input type="radio" name="rating-1" class="mask mask-star" />
-                                    <input type="radio" name="rating-1" class="mask mask-star" checked="checked" />
-                                    <input type="radio" name="rating-1" class="mask mask-star" />
-                                    <input type="radio" name="rating-1" class="mask mask-star" />
-                                    <input type="radio" name="rating-1" class="mask mask-star" />
+                                <div class="flex">
+                                    <i class="fa-regular fa-heart ms-1"></i>
+                                    <i class="fa-regular fa-heart ms-1"></i>
+                                    <i class="fa-regular fa-heart ms-1"></i>
+                                    <i class="fa-regular fa-heart ms-1"></i>
+                                    <i class="fa-regular fa-heart ms-1"></i>
                                 </div>
                             </td>
                             <td>
@@ -102,4 +123,8 @@
 </div>
 </template>
 <style scoped>
+@import '@fortawesome/fontawesome-free/css/fontawesome.css';
+@import '@fortawesome/fontawesome-free/css/regular.css';
+@import '@fortawesome/fontawesome-free/css/solid.css';
+@import '@fortawesome/fontawesome-free/css/brands.css';
 </style>
