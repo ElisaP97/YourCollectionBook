68fabd9b7f00f13bb3392f1c8dc48a2cc2baf2e1
Elisa Poggi <elisapoggi97@gmail.com>
2024-07-03 02:43:00 +0200

    Aggiunta funzione aggiungi libro ed elimina libro, lavori in corso sulla modifica

--- src/components/Collection.vue
+++ src/components/Collection.vue
@@ -46,6 +46,64 @@
         })
     }
 
+    
+    // form per aggiunta libro 
+    const addTitle= ref("");
+    const addAuthor= ref("");
+    const addTotVolumes= ref("");
+    const addMyVolumes= ref("");
+    const addStatus= ref("");
+    const addReading= ref("");
+    const addRaiting= ref("");
+
+    function addBook(){
+        let title = addTitle._value;
+        let author = addAuthor._value;
+        let totVolumes = addTotVolumes._value;
+        let myVolumes = addMyVolumes._value;
+        let status = addStatus._value;
+        let reading = parseInt(addReading._value);
+        let raiting = parseInt(addRaiting._value);
+
+    //console.log(title,author,totVolumes,myVolumes,status,reading,raiting);
+
+        const newBook = {
+            reading : reading,
+            title : title,
+            author: author,
+            volumes: `${myVolumes}/${totVolumes}`,
+            status: status,
+            rating: raiting,
+        }
+
+        axios.post(urlCollection, newBook).then(resp=>{
+            books.value = [...books.value, resp.data]; 
+        })
+
+        // reset campi input
+        addTitle._value= " ";
+        addAuthor._value= " ";
+        addTotVolumes._value= " ";
+        addMyVolumes._value= " ";
+        addStatus._value= " ";
+        addReading._value= " ";
+        addRaiting._value= " ";
+    }
+    
+    function deleteBook(id){
+        axios.delete(`${urlCollection}/${id}`).then(resp=>{
+        //come per l'aggiunta anche per l'eliminazione andiamo ad aggiornare la lista dei prodotti
+        //per farlo filtriamo l'arrey andando a dire di prendere tutti i prodotti con id diverso da quello eliminato
+            books.value = books.value.filter(p => p.id != id); 
+        });
+    }
+
+    function editBook(id){
+        axios.get(`${urlCollection}/${id}`).then(resp=>{
+
+
+        });
+    }
 
 </script>
 
@@ -84,28 +142,28 @@
                 </button>
             </div>
 
-            <div v-if="addBooks===2" class="bg-secondary-content border border-secondary border-4 w-4/5 h-3/4 2xl:h-3/4 mt-5 p-5 grid gap-5 grid-cols-3 grid-rows-4 items-center">
+            <div v-if="addBooks===2 || addBooks===3" class="bg-secondary-content border border-secondary border-4 w-4/5 h-3/4 2xl:h-3/4 mt-5 p-5 grid gap-5 grid-cols-3 grid-rows-4 items-center">
                 <!--Prima riga-->
-                <input type="text" placeholder="Titolo" class="input input-xs lg:input-md input-bordered input-secondary w-full max-w-xs row-start-1"/>
-                <input type="text" placeholder="Autore" class="input input-xs lg:input-md input-bordered input-secondary w-full max-w-xs row-start-1" />
+                <input type="text"  v-model.trim="addTitle" placeholder="Titolo" class="input input-xs lg:input-md input-bordered input-secondary w-full max-w-xs row-start-1"/>
+                <input type="text" v-model.trim="addAuthor" placeholder="Autore" class="input input-xs lg:input-md input-bordered input-secondary w-full max-w-xs row-start-1" />
                 <!--Seconda riga-->
                 <label class="form-control w-full max-w-xsol-span-1 row-start-2">
                 <div class="label">
                     <span class="label-text text-xs lg:text-lg">Volumi in possesso</span>
                 </div>
-                <input type="number" min="1" class="input input-xs lg:input-md input-bordered w-full max-w-xs" />
+                <input type="number" v-model.trim="addMyVolumes" min="1" class="input input-xs lg:input-md input-bordered w-full max-w-xs" />
                 </label>
                 <label class="form-control w-full max-w-xsol-span-1 row-start-2">
                 <div class="label">
                     <span class="label-text text-xs lg:text-lg">Volumi in totale</span>
                 </div>
-                <input type="number" min="1" class="input input-xs lg:input-md input-bordered w-full max-w-xs" />
+                <input type="number" v-model.trim="addTotVolumes" min="1" class="input input-xs lg:input-md input-bordered w-full max-w-xs" />
                 </label>
                 <label class="form-control w-full max-w-xsol-span-1 row-start-2">
                 <div class="label">
                     <span class="label-text text-xs lg:text-lg">Status Opera</span>
                 </div>
-                <select class="select select-xs lg:select-md select-secondary w-full max-w-xs col-span-1 row-start-2">
+                <select v-model="addStatus" class="select select-xs lg:select-md select-secondary w-full max-w-xs col-span-1 row-start-2">
                     <option class="text-xs lg:text-lg">Completa</option>
                     <option class="text-xs lg:text-lg">In Corso</option>
                     <option class="text-xs lg:text-lg">Droppata</option>
@@ -116,10 +174,10 @@
                 <div class="label">
                     <span class="label-text text-xs lg:text-lg">Stato lettura</span>
                 </div>
-                <select class="select select-xs lg:select-md select-secondary w-full max-w-xs col-span-1 row-start-3">
-                    <option>Completa</option>
-                    <option>In Corso</option>
-                    <option>Non iniziata</option>
+                <select v-model="addReading" class="select select-xs lg:select-md select-secondary w-full max-w-xs col-span-1 row-start-3">
+                    <option value="1">Completa</option>
+                    <option value="2">In Corso</option>
+                    <option value="3">Non iniziata</option>
                 </select>
                 </label>
                 <label class="form-control w-full max-w-xsol-span-1 row-start-3">
@@ -127,16 +185,22 @@
                     <span class="label-text text-xs lg:text-lg">Raiting</span>
                 </div>
                 <div class="rating col-span-2 row-start-3">
-                    <input type="radio" name="rating-1" class="mask mask-heart ms-1 bg-secondary" />
-                    <input type="radio" name="rating-1" class="mask mask-heart ms-1 bg-secondary" />
-                    <input type="radio" name="rating-1" class="mask mask-heart ms-1 bg-secondary" />
-                    <input type="radio" name="rating-1" class="mask mask-heart ms-1 bg-secondary" />
-                    <input type="radio" name="rating-1" class="mask mask-heart ms-1 bg-secondary" />
+                    <input type="radio" name="rating-1" value="1" v-model="addRaiting" class="mask mask-heart ms-1 bg-secondary" />
+                    <input type="radio" name="rating-1" value="2" v-model="addRaiting" class="mask mask-heart ms-1 bg-secondary" />
+                    <input type="radio" name="rating-1" value="3" v-model="addRaiting" class="mask mask-heart ms-1 bg-secondary" />
+                    <input type="radio" name="rating-1" value="4" v-model="addRaiting" class="mask mask-heart ms-1 bg-secondary" />
+                    <input type="radio" name="rating-1" value="5" v-model="addRaiting" class="mask mask-heart ms-1 bg-secondary" />
                 </div>
                 </label>
                 <!--Quarta riga-->
-                <button class="btn btn-xs lg:btn-md btn-secondary mt-5 col-span-3 row-start-4">
-                    Aggiungi Prodotto
+                <button v-if="addBooks===2" @click="addBook(), addBooks=1" class="btn btn-xs lg:btn-md btn-secondary mt-5 col-span-3 row-start-4">
+                    Aggiungi Serie
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+                        <path fill-rule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 9a.75.75 0 0 0-1.5 0v2.25H9a.75.75 0 0 0 0 1.5h2.25V15a.75.75 0 0 0 1.5 0v-2.25H15a.75.75 0 0 0 0-1.5h-2.25V9Z" clip-rule="evenodd" />
+                    </svg>
+                </button>
+                <button v-if="addBooks===3" @click="addBook(), addBooks=1" class="btn btn-xs lg:btn-md btn-secondary mt-5 col-span-3 row-start-4">
+                    Modifica Serie
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
                         <path fill-rule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 9a.75.75 0 0 0-1.5 0v2.25H9a.75.75 0 0 0 0 1.5h2.25V15a.75.75 0 0 0 1.5 0v-2.25H15a.75.75 0 0 0 0-1.5h-2.25V9Z" clip-rule="evenodd" />
                     </svg>
@@ -208,7 +272,14 @@
                                 </div>
                             </td>
                             <td>
-                            <button class="btn btn-ghost btn-xs">Modifica</button>
+                            <div class="flex justify-around">
+                                <button @click="addBooks=3,editBook(book.id)" class="btn btn-secondary btn-outline border-none btn-xs">Modifica</button>
+                                <button @click="deleteBook(book.id)" class="btn btn-error btn-outline border-none btn-xs" >
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                                    </svg>
+                                </button>
+                            </div>
                             </td>
                         </tr>
                         
